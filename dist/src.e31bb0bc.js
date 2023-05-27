1de94b185bdbe8818317788123f848c2fa8eee86
chraul22 <c.auld@student.curtin.edu.au>
2023-05-27 18:25:42 +1000

    first commit

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -15378,7 +15378,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60608" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62019" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
